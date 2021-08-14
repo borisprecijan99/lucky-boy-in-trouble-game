ac10b04f2f7f1570f1df8d60c9ba5ac8d8294fb2
borisprecijan99 <borisprecijanbp@gmail.com>
2021-08-14 03:08:01 +0200

    Added script for platform movement and partially completed secend level

--- Assets/Scripts/PlatformMovement.cs
+++ Assets/Scripts/PlatformMovement.cs
@@ -1,18 +1,64 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlatformMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Vector3 from;
+
+    [SerializeField]
+    private Vector3 to;
+    private Vector3 currentTarget;
+    private float speed, delayStart, delayTime, tolerance;
+ 
     void Start()
     {
-        
+        delayTime = 1f;
+        speed = 1f;
+        from = transform.position;
+        currentTarget = from;
+        tolerance = speed * Time.deltaTime;
+    }
+ 
+    void FixedUpdate()
+    {
+        if (transform.position != currentTarget)
+        {
+            MovePlatform();
+        }
+        else
+        {
+            UpdateTarget();
+        }
+    }
+ 
+    private void MovePlatform()
+    {
+        Vector3 heading = currentTarget - transform.position;
+        transform.position += (heading / heading.magnitude) * speed * Time.deltaTime;
+        if (heading.magnitude < tolerance)
+        {
+            transform.position = currentTarget;
+            delayStart = Time.time;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void UpdateTarget()
     {
-        
+        if (Time.time - delayStart > delayTime)
+        {
+             if (currentTarget == from)
+                currentTarget = to;
+            else
+                currentTarget = from;
+        }
+    }
+ 
+    void OnTriggerEnter(Collider other)
+    {
+        other.transform.SetParent(transform);
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        other.transform.SetParent(null);
     }
 }
